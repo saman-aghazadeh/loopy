95c731f46c4a81cde0a9dd66c09a299957ec8351
saman biookaghazadeh - sbiookag@asu.edu -  - Grad Research Associate - Graduate Student - Student - Sch Compt Infor Sbiookag Dec Sys Engr <saman.aghazadeh@gmail.com>
2018-12-20 07:58:46 -0700

    2mm

--- src/opencl/PolyBench/2mm.cl
+++ src/opencl/PolyBench/2mm.cl
@@ -24,11 +24,49 @@ __kernel void mm_k1 (__global const DTYPE* restrict A,
 #ifdef FPGA_SINGLE
                    ,const int lll)
 #else
-                   )
+                   ,const int lll
+                   ,__global DTYPE* restrict temp)
 #endif
 {
 
 #ifdef GPU
+
+      const int row = get_local_id(0);
+      const int col = get_local_id(1);
+      const int globalRow = 32 * get_group_id(0) + row;
+      const int globalCol = 32 * get_group_id(1) + col;
+
+      // Local memory to fit a tile of 32*32 elements of A and B
+      __local float Asub[32][32];
+      __local float Bsub[32][32];
+
+      // Initialize the accumulation register
+      float acc = 0.0f;
+
+      // Loop over all tiles
+      const int numTiles = lll/32;
+      for (int t = 0; t < numTiles; t++) {
+
+          // Load one tile of A and B into local memory
+          const int tiledRow = 32*t + row;
+          const int tiledCol = 32*t + col;
+          Asub[col][row] = A[tiledCol*M + globalRow];
+          Bsub[col][row] = B[globalCol*K + tiledRow];
+
+          // Synchronize to make sure the tile is loaded
+          barrier(CLK_LOCAL_MEM_FENCE);
+
+          // Perform the computation for a single tile
+          for (int k = 0; k < 32; k++) {
+              acc += Asub[k][row] * Bsub[col][k];
+          }
+
+          // Synchronization before loading the next tile
+          barrier(CLK_LOCAL_MEM_FENCE);
+      }
+
+      // Store the final result in C
+      temp[globalCol*M + globalRow] = acc;
 
 #endif
 
@@ -80,50 +118,86 @@ __kernel void mm_k2 (__global const DTYPE* restrict A,
 #ifdef FPGA_SINGLE
                       ,const int lll)
 #else
-                      )
+                      ,const int lll
+                      ,__global DTYPE* restrict temp)
 #endif
 {
 
 #ifdef GPU
 
-#endif
+       const int row = get_local_id(0);
+       const int col = get_local_id(1);
+       const int globalRow = 32 * get_group_id(0) + row;
+       const int globalCol = 32 * get_group_id(1) + col;
 
+       // Local memory to fit a tile of 32*32 elements of A and B
+       __local Tsub[32][32];
+       __local Csub[32][32];
+
+       // Initialize the accumulation register
+       float acc = 0.0f;
+
+       // Loop over all tiles
+       const int numTiles = lll/32;
+       for (int t = 0; t < numTiles; t++) {
+
+           // Load one tile of A and B into local memory
+           const int tiledRow = 32*t + row;
+           const int tiledCol = 32*t + col;
+           Tsub[col][row] = temp[tiledCol*M + globalRow];
+           Csub[col][row] = C[globalCol*K + tiledRow];
+
+           // Synchronize to make sure the tile is loaded
+           barrier (CLK_LOCAL_MEM_FENCE);
+
+           // Perform the computation for a single tile
+           for (int k = 0; k < 32; k++) {
+               acc += Asub[k][row] * Bsub[col][k];
+           }
+
+           // Synchronization before loading the next tile
+           barrier (CLK_LOCAL_MEM_FENCE);
+       }
+
+       D[globalCol*M + globalRow] = acc;
+
+#endif
 
 #ifdef FPGA_SINGLE
        for (int i = 0; i < lll; i++) {
            for (int j = 0; j < lll; j++) {
                DTYPE temp = read_channel_altera(c0);
 
-			   #pragma ivdep	
-			   for (int z = 0; z < lll/BLOCK_SIZE; z++) {
-					DTYPE C_local[BLOCK_SIZE];
-					DTYPE D_local[BLOCK_SIZE];
+			         #pragma ivdep	
+			         for (int z = 0; z < lll/BLOCK_SIZE; z++) {
+					         DTYPE C_local[BLOCK_SIZE];
+					         DTYPE D_local[BLOCK_SIZE];
 				
-					// Coalescing memory read from the memory section "A"
-					#pragma unroll
-					for (int k = 0; k < BLOCK_SIZE; k++) {
-						C_local[k] = C[j*lll+z*BLOCK_SIZE+k];
-					}
+                   // Coalescing memory read from the memory section "A"
+					         #pragma unroll
+					         for (int k = 0; k < BLOCK_SIZE; k++) {
+						           C_local[k] = C[j*lll+z*BLOCK_SIZE+k];
+					         }
 					
-					// Initializing the memory section "D"
-					#pragma unroll
-					for (int k = 0; k < BLOCK_SIZE; k++) {
-						D_local[k] = 0.0;
-					}
+                   // Initializing the memory section "D"
+					         #pragma unroll
+					         for (int k = 0; k < BLOCK_SIZE; k++) {
+						           D_local[k] = 0.0;
+					         }
 
-					// Accumulating the result of multiplications
-					#pragma unroll
-               		for (int k = 0; k < BLOCK_SIZE; k++) {
-                  		D_local[k] += temp * C_local[k];
-               		}
+					         // Accumulating the result of multiplications
+					         #pragma unroll
+               		 for (int k = 0; k < BLOCK_SIZE; k++) {
+                       D_local[k] += temp * C_local[k];
+               		 }
 
-					// final accumulation
-					#pragma unroll
-					for (int k = 0; k < BLOCK_SIZE; k++) {
-						D[j*lll+z*BLOCK_SIZE+k] += D_local[k];
-					}
+					         // final accumulation
+					         #pragma unroll
+					         for (int k = 0; k < BLOCK_SIZE; k++) {
+						           D[j*lll+z*BLOCK_SIZE+k] += D_local[k];
+					         }
 
-			   }
+			         }
 				
            }
        }
---
+++ src/opencl/PolyBench/2mm.cpp
@@ -0,0 +1,607 @@
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <cassert>
+#include <chrono>
+
+#include "OpenCLDeviceInfo.h"
+#include "OptionParser.h"
+#include "support.h"
+#include "Event.h"
+#include "ProgressBar.h"
+#include "ResultDatabase.h"
+#include "aocl_utils.h"
+
+using namespace std;
+
+#define CL_BAIL_ON_ERROR(err) \
+{															\
+ 		CL_CHECK_ERROR(err);			\
+		if (err != CL_SUCCESS)		\
+      return;									\
+}
+
+// Forward declaration
+template <class T> inline std::string toString (const T& t) {
+  std::stringstream ss;
+  ss << t;
+  return ss.str();
+}
+
+void addBenchmarkSpecOptions (OptionParser &op) {
+	op.addOption ("min_data_size", OPT_INT, "0", "minimum data size (in Kilobytes)");
+  op.addOption ("max_data_size", OPT_INT, "0", "maximum data size (in Kilobytes)");
+  op.addOption ("data_type", OPT_STRING, "", "data type (INT or SINGLE or DOUBLE)");
+  op.addOption ("kern_loc", OPT_STRING, "", "path to the kernel");
+  op.addOption ("kern1_name", OPT_STRING, "", "name of the first kernel function");
+  op.addOption ("kern2_name", OPT_STRING, "", "name of the second kernel function");
+  op.addOption ("device_type", OPT_STRING, "", "device type (GPU or FPGA)");
+  op.addOption ("fpga_op_type", OPT_STRING, "", "FPGA TYPE (NDRANGE or SINGLE)");
+  op.addOption ("intensity", OPT_STRING, "", "Setting intensity of the computation");
+  op.addOption ("use_channel", OPT_INT, "0", "Whether using channel or not");
+}
+
+void RunBenchmark (cl_device_id dev,
+                   cl_context ctx,
+                   cl_command_queue queue,
+                   ResultDatabase &resultDB,
+                   OptionParser &op) {
+
+	cl_int status;
+  cl_command_queue second_queue;
+
+  second_queue = clCreateCommandQueue (ctx, dev, CL_QUEUE_PROFILING_ENABLE, &status);
+  CL_CHECK_ERROR (status);
+
+	string int_precision = "-DINT_PRECISION ";
+  string single_precision = "-DSINGLE_PRECISION ";
+  string double_precision = "-DDOUBLE_PRECISION ";
+
+	long long unsigned  minDataSize = op.getOptionInt("min_data_size") * 1024 * 1024;
+	long long unsigned  maxDataSize = op.getOptionInt("max_data_size") * 1024 * 1024;
+
+  int passes = op.getOptionInt("passes");
+
+	string dataType = op.getOptionString("data_type");
+	string kernel_location = op.getOptionString("kern_loc");
+  string kernel1_name = op.getOptionString("kern1_name");
+  string kernel2_name = op.getOptionString("kern2_name");
+	string device_type = op.getOptionString("device_type");
+  string fpga_op_type = op.getOptionString("fpga_op_type");
+	string flags = "";
+  string intensity = op.getOptionString("intensity");
+  int use_channel = op.getOptionInt("use_channel");
+
+	int localX = 32;
+  int localY = 32;
+  int globalX = 0;
+  int globalY = 0;
+
+	cl_int err;
+  cl_program program;
+
+  Event evKernel1 ("EventKernel1");
+  Event evKernel2 ("EventKernel2");
+  Event evRead ("EventRead");
+
+	cout << "[INFO] Device Type is " << device_type << endl;
+  cout << "[INFO] Data Type is " << dataType << endl;
+  cout << "[INFO] Kernel Location is " << kernel_location << endl;
+  cout << "[INFO] kernel one is " << kernel1_name << endl;
+	cout << "[INFO] kernel two is " << kernel2_name << endl;
+  cout << "[INFO] Minimum Data Size is " << minDataSize << endl;
+  cout << "[INFO] Maximum Data Size is " << maxDataSize << endl;
+	cout << "[INFO] Number of passes is " << passes << endl;
+	cout << "[INFO] use channel is " << use_channel << endl;
+
+  // First building the program
+
+  flags += "-cl-opt-disable ";
+
+	if (intensity == "1") {
+    flags += "-DINTENSITY1 ";
+  } else if (intensity == "2") {
+    flags += "-DINTENSITY2 ";
+  } else if (intensity == "3") {
+    flags += "-DINTENSITY3 ";
+  } else if (intensity == "4") {
+    flags += "-DINTENSITY4 ";
+  } else if (intensity == "5") {
+    flags += "-DINTENSITY5 ";
+  } else if (intensity == "6") {
+    flags += "-DINTENSITY6 ";
+  } else if (intensity == "7") {
+    flags += "-DINTENSITY7 ";
+  } else if (intensity == "8") {
+    flags += "-DINTENSITY8 ";
+  }
+
+	if (device_type == "GPU") {
+    flags += "-DGPU ";
+  } else if (device_type == "FPGA") {
+    if (fpga_op_type == "NDRANGE") {
+      flags += "-DFPGA_NDRANGE ";
+    } else if (fpga_op_type == "SINGLE") {
+      flags += "-DFPGA_SINGLE ";
+    }
+  }
+
+	if (dataType == "INT") {
+    flags += int_precision;
+  } else if (dataType == "SINGLE") {
+    flags += single_precision;
+  } else if (dataType == "DOUBLE") {
+    flags += double_precision;
+  }
+
+	if (device_type == "GPU") {
+  	ifstream kernelFile (kernel_location, ios::in);
+    if (!kernelFile.is_open()) {
+      cerr << "[ERROR] Failed to open file" << kernel_location << "for reading!" << endl;
+      exit (0);
+    }
+
+    ostringstream oss;
+    oss << kernelFile.rdbuf();
+
+    string srcStdStr = oss.str();
+    const char* srcStr = srcStdStr.c_str();
+
+    program = clCreateProgramWithSource (ctx, 1, (const char **)&srcStr, NULL, &err);
+    CL_CHECK_ERROR (err);
+  } else if (device_type == "FPGA") {
+    string binary_file = aocl_utils::getBoardBinaryFile (kernel_location.c_str(), dev);
+    program = aocl_utils::createProgramFromBinary (ctx, binary_file.c_str(), &dev, 1);
+  }
+	cout << "[INFO] Program Created Successfully!" << endl;
+
+  err = clBuildProgram (program, 1, &dev, flags.c_str(), NULL, NULL);
+  cout << "[INFO] Kernel compiled with flags " << flags << endl;
+
+  if (err != 0) {
+    char log[5000];
+    size_t retSize = 0;
+    err = clGetProgramBuildInfo (program, dev, CL_PROGRAM_BUILD_LOG,
+                                 5000 * sizeof (char), log, &retSize);
+
+    cout << "[ERROR] Build Error!" << endl;
+    cout << "[ERROR] Ret Size: " << retSize << endl;
+    cout << "[ERROR] Log: " << log << endl;
+    exit (0);
+  }
+
+  CL_CHECK_ERROR (err);
+
+  for (unsigned long long dataSize = minDataSize; dataSize <= maxDataSize; dataSize *= 2) {
+
+		cout << "[INFO] data size is " << dataSize << endl;
+
+    void *A, *B, *C;
+    cl_mem clA, clB, clC, clD, clTemp;
+
+    cl_kernel kernel1 = clCreateKernel (program, kernel1_name.c_str(), &err);
+    CL_CHECK_ERROR (err);
+
+    cl_kernel kernel2 = clCreateKernel (program, kernel2_name.c_str(), &err);
+    CL_CHECK_ERROR (err);
+
+    A = (void *) malloc (dataSize);
+    B = (void *) malloc (dataSize);
+    C = (void *) malloc (dataSize);
+
+		int size = 0;
+		if (dataType == "INT")
+      size = (int)(dataSize / sizeof(int));
+    else if (dataType == "SINGLE")
+      size = (int)(dataSize / sizeof(float));
+    else if (dataType == "DOUBLE")
+      size = (int)(dataSize / sizeof(double));
+
+    if (dataType == "INT") {
+      for (int i = 0; i < size; i++) {
+      	((int *)A)[i] = 10;
+        ((int *)B)[i] = 10;
+        ((int *)C)[i] = 10;
+      }
+    } else if (dataType == "SINGLE") {
+      for (int i = 0; i < size; i++) {
+        ((float *)A)[i] = 10;
+        ((float *)B)[i] = 10;
+        ((float *)C)[i] = 10;
+      }
+    } else if (dataType == "DOUBLE") {
+      for (int i = 0; i < size; i++) {
+        ((double *)A)[i] = 10;
+        ((double *)B)[i] = 10;
+        ((double *)C)[i] = 10;
+      }
+    }
+
+    clA = clCreateBuffer (ctx, CL_MEM_READ_ONLY, dataSize, NULL, &err);
+    CL_CHECK_ERROR (err);
+
+    clB = clCreateBuffer (ctx, CL_MEM_READ_ONLY, dataSize, NULL, &err);
+    CL_CHECK_ERROR (err);
+
+    clC = clCreateBuffer (ctx, CL_MEM_READ_ONLY, dataSize, NULL, &err);
+    CL_CHECK_ERROR (err);
+
+    clD = clCreateBuffer (ctx, CL_MEM_READ_WRITE, dataSize, NULL, &err);
+    CL_CHECK_ERROR (err);
+
+    clTemp = clCreateBuffer (ctx, CL_MEM_READ_WRITE, dataSize, NULL, &err);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (queue, clA, CL_TRUE, 0, dataSize, A, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (queue, clB, CL_TRUE, 0, dataSize, B, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (queue, clC, CL_TRUE, 0, dataSize, C, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (second_queue, clA, CL_TRUE, 0, dataSize, A, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (second_queue, clB, CL_TRUE, 0, dataSize, B, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clEnqueueWriteBuffer (second_queue, clC, CL_TRUE, 0, dataSize, C, 0, NULL, NULL);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel1, 0, sizeof (cl_mem), (void *) &clA);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel2, 0, sizeof (cl_mem), (void *) &clA);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel1, 1, sizeof (cl_mem), (void *) &clB);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel2, 1, sizeof (cl_mem), (void *) &clB);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel1, 2, sizeof (cl_mem), (void *) &clC);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel2, 2, sizeof (cl_mem), (void *) &clC);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel1, 3, sizeof (cl_mem), (void *) &clD);
+    CL_CHECK_ERROR (err);
+
+    err = clSetKernelArg (kernel2, 3, sizeof (cl_mem), (void *) &clD);
+    CL_CHECK_ERROR (err);
+
+    if (dataType == "INT") {
+      int alpha = 0;
+      int beta = 0;
+
+      err = clSetKernelArg (kernel1, 4, sizeof (int), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 4, sizeof (int), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel1, 5, sizeof (int), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 5, sizeof (int), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+    } else if (dataType == "SINGLE") {
+      float alpha = 0;
+      float beta = 0;
+
+      err = clSetKernelArg (kernel1, 4, sizeof (float), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 4, sizeof (float), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel1, 5, sizeof (float), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 5, sizeof (float), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+    } else if (dataType == "DOUBLE") {
+      double alpha = 0;
+      double beta = 0;
+
+      err = clSetKernelArg (kernel1, 4, sizeof (double), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 4, sizeof (double), (void *) &alpha);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel1, 5, sizeof (double), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+      err = clSetKernelArg (kernel2, 5, sizeof (double), (void *) &beta);
+      CL_CHECK_ERROR (err);
+
+    }
+
+
+    if (device_type == "FPGA") {
+      if (fpga_op_type == "SINGLE") {
+        int numIterations = 0;
+        int lll = 0;
+        if (dataType == "INT") {
+          numIterations = dataSize / 4;
+        } else if (dataType == "SINGLE") {
+          numIterations = dataSize / 4;
+        } else if (dataType == "DOUBLE") {
+          numIterations = dataSize / 8;
+        }
+
+        lll = sqrt(numIterations);
+        if (lll * lll != numIterations) continue;
+
+        if (device_type == "GPU") {
+          err = clSetKernelArg (kernel1, 6, sizeof (int), &lll);
+          CL_CHECK_ERROR (err);
+
+          err = clSetKernelArg (kernel2, 6, sizeof (int), &lll);
+          CL_CHECK_ERROR (err);
+
+          err = clSetKernelArg (kernel1, 7, sizeof (cl_mem), (void *) &clTemp);
+          CL_CHECK_ERROR (err);
+
+          err = clSetKernelArg (kernel2, 7, sizeof (cl_mem), (void *) &clTemp);
+          CL_CHECK_ERROR (err);
+        } else if (device_type == "FPGA"){
+          err = clSetKernelArg (kernel1, 6, sizeof (int), &lll);
+          CL_CHECK_ERROR (err);
+
+          err = clSetKernelArg (kernel2, 6, sizeof (int), &lll);
+          CL_CHECK_ERROR (err);
+        }
+
+      }
+    }
+
+		clFinish (queue);
+    clFinish (second_queue);
+    CL_BAIL_ON_ERROR (err);
+
+
+		size_t global_work_size[2];
+    if (dataType == "INT") {
+      global_work_size[0] = sqrt(dataSize/4);
+      global_work_size[1] = sqrt(dataSize/4);
+    } else if (dataType == "SINGLE") {
+      global_work_size[0] = sqrt(dataSize/4);
+      global_work_size[1] = sqrt(dataSize/4);
+    } else if (dataType == "DOUBLE") {
+      global_work_size[0] = sqrt(dataSize/8);
+      global_work_size[1] = sqrt(dataSize/8);
+    }
+
+    // global_work_size[0] = global_work_size[0] / 2;
+		cout << "[INFO] global work size is " << global_work_size[0] << "," << global_work_size[1] << endl;
+
+    const size_t local_work_size[] = {(size_t)localX, (size_t)localY};
+		cout << "[INFO] local work size is " << local_work_size[0] << "," << local_work_size[1] << endl;
+
+		Event evKernel1 ("KernelEvent1");
+    Event evKernel2 ("KernelEvent2");
+    long count;
+
+    if (device_type == "FPGA") {
+      err = clEnqueueTask (second_queue, kernel2, 0, NULL, &evKernel2.CLEvent());
+      err = clEnqueueTask (queue, kernel1, 0, NULL, &evKernel1.CLEvent());
+    } else {
+
+      auto start = std::chrono::high_resolution_clock::now();
+       	err = clEnqueueNDRangeKernel (queue, kernel1, 2,
+                                      NULL, global_work_size, local_work_size,
+                                      0, NULL, &evKernel1.CLEvent());
+        err = clEnqueueNDRangeKernel (second_queue, kernel2, 2,
+                                      NULL, global_work_size, local_work_size,
+                                      1, &evKernel1.CLEvent(), &evKernel2.CLEvent());
+    }
+
+    clFinish (queue);
+    clFinish (second_queue);
+    CL_BAIL_ON_ERROR (err);
+
+		cout << "[INFO] Done with warmup" << endl;
+
+		for (int iter = 0; iter < passes; iter++) {
+
+			if (dataType == "INT") {
+        for (int i = 0; i < size; i++) {
+          ((int *)A)[i] = 10;
+          ((int *)B)[i] = 10;
+          ((int *)C)[i] = 10;
+        }
+      } else if (dataType == "SINGLE") {
+        for (int i = 0; i < size; i++) {
+          ((float *)A)[i] = 10;
+          ((float *)B)[i] = 10;
+          ((float *)C)[i] = 10;
+        }
+      } else if (dataType == "DOUBLE") {
+        for (int i = 0; i < size; i++) {
+          ((double *)A)[i] = 10;
+          ((double *)B)[i] = 10;
+          ((double *)C)[i] = 10;
+        }
+      }
+
+      long count = 0;
+      if (device_type == "FPGA") {
+        err = clEnqueueTask (second_queue, kernel2, 0, NULL, &evKernel2.CLEvent());
+        err = clEnqueueTask (queue, kernel1, 0, NULL, &evKernel1.CLEvent());
+      } else {
+       	err = clEnqueueNDRangeKernel (queue, kernel1, 2,
+                                      NULL, global_work_size, local_work_size,
+                                      0, NULL, &evKernel1.CLEvent());
+        err = clEnqueueNDRangeKernel (second_queue, kernel2, 2,
+                                      NULL, global_work_size, local_work_size,
+                                      1, &evKernel1.CLEvent(), &evKernel2.CLEvent());
+      }
+
+      clFinish(queue);
+      clFinish(second_queue);
+      CL_BAIL_ON_ERROR (err);
+
+			cl_ulong totalTime = 0;
+
+			evKernel1.FillTimingInfo();
+      evKernel2.FillTimingInfo();
+
+      if (device_type == "FPGA") {
+        cl_ulong start1 = evKernel1.SubmitTime();
+       	cl_ulong start2 = evKernel2.SubmitTime();
+
+        cl_ulong end1 = evKernel1.EndTime();
+        cl_ulong end2 = evKernel2.EndTime();
+
+        cl_ulong start = (start1 > start2) ? start2 : start1;
+        cl_ulong end = (end1 > end2) ? end2 : end1;
+
+        totalTime = end - start;
+      } else {
+        cl_ulong start = evKernel1.SubmitTime();
+        cl_ulong end = evKernel2.EndTime();
+				totalTime = end - start;
+      }
+
+      if (dataType == "INT")
+      	resultDB.AddResult ("KernelINT" /*+ toString(dataSize) + "KiB"*/,
+                          	toString(dataSize), "Bytes", totalTime);
+      else if (dataType == "SINGLE")
+        resultDB.AddResult ("KernelSINGLE",
+                            toString(dataSize), "Bytes", totalTime);
+
+      err = clEnqueueReadBuffer (queue, clA, CL_TRUE, 0, dataSize, A, 0, 0, &evRead.CLEvent());
+
+      clFinish (queue);
+      CL_BAIL_ON_ERROR (err);
+
+			continue;
+
+			void* ACPU = (void *) (malloc (dataSize));
+      void* BCPU = (void *) (malloc (dataSize));
+      void* CCPU = (void *) (malloc (dataSize));
+      void* DCPU = (void *) (malloc (dataSize));
+      void* ECPU = (void *) (malloc (dataSize));
+			void* BPrimeCPU = (void *) (malloc(dataSize));
+
+      if (dataType == "INT") {
+        for (int i = 0; i < size+2; i++) {
+          ((int *)ACPU)[i] = 1;
+          ((int *)BCPU)[i] = 1;
+          ((int *)CCPU)[i] = 1;
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((int *)BPrimeCPU)[j+1] = ((int *)BCPU)[j+2] - ((int *)ECPU)[j+1] * ((int *)DCPU)[j+1];
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((int *)ACPU)[j+1] = ((int *)BPrimeCPU)[j] - ((int *)ECPU)[j+1] * ((int *)DCPU)[j+1];
+        }
+
+        int wrong = 0;
+
+        for (int i = 0; i < size; i++) {
+          if (((int *)ACPU)[i] != ((int *)A)[i]) {
+            wrong = 2;
+            break;
+          }
+        }
+
+        if (wrong == 1) cout << "[ERROR] B does not match!" << endl;
+        else if (wrong == 2) cout << "[ERROR] A does not match!" << endl;
+        else cout << "[INFO] Data matches perfectly!" << endl;
+      } else if (dataType == "SINGLE") {
+        for (int i = 0; i < size; i++) {
+          ((float *)ACPU)[i] = 1;
+          ((float *)BCPU)[i] = 1;
+          ((float *)CCPU)[i] = 1;
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((float *)BPrimeCPU)[j+1] = ((float *)BCPU)[j+2] - ((float *)ECPU)[j+1] * ((float *)DCPU)[j+1];
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((float *)ACPU)[j+1] = ((float *)BPrimeCPU)[j] - ((float *)ECPU)[j+1] * ((float *)DCPU)[j+1];
+        }
+
+        int wrong = 0;
+
+        for (int i = 0; i < size; i++) {
+          if (((int *)ACPU)[i] != ((int *)A)[i]) {
+            wrong = 2;
+            break;
+          }
+        }
+
+        if (wrong == 1) cout << "[ERROR] B does not match!" << endl;
+        else if (wrong == 2) cout << "[ERROR] A does not match!" << endl;
+        else cout << "[INFO] Data matches perfectly!" << endl;
+
+      } else if (dataType == "DOUBLE") {
+        for (int i = 0; i < size; i++) {
+          ((double *)ACPU)[i] = 1;
+          ((double *)BCPU)[i] = 1;
+          ((double *)BPrimeCPU)[i] = 1;
+          ((double *)CCPU)[i] = 1;
+          ((double *)DCPU)[i] = 1;
+          ((double *)ECPU)[i] = 1;
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((double *)BPrimeCPU)[j+1] = ((double *)BCPU)[j+2] - ((double *)ECPU)[j+1] * ((double *)DCPU)[j+1];
+        }
+
+        for (int j = 0; j < size; j++) {
+          ((double *)ACPU)[j+1] = ((double *)BPrimeCPU)[j] - ((double *)ECPU)[j+1] * ((double *)DCPU)[j+1];
+        }
+
+        int wrong = 0;
+
+        for (int i = 0; i < size; i++) {
+          if (((double *)ACPU)[i] != ((double *)A)[i]) {
+            wrong = 2;
+            break;
+          }
+        }
+
+        if (wrong == 1) cout << "[ERROR] B does not match!" << endl;
+        else if (wrong == 2) cout << "[ERROR]AA does not match!" << endl;
+        else cout << "[INFO] Data matches perfectly!" << endl;
+
+      }
+
+      free (ACPU);
+      free (BCPU);
+			free (CCPU);
+			free (DCPU);
+			free (ECPU);
+			free (BPrimeCPU);
+
+    }
+
+    clReleaseMemObject (clA);
+    clReleaseMemObject (clB);
+    clReleaseMemObject (clC);
+    clReleaseKernel (kernel1);
+    clReleaseKernel (kernel2);
+    free (A);
+    free (B);
+    free (C);
+  }
+}
+
+void cleanup () {
+  
+}
